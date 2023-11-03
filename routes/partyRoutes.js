b8fb6674faa19874ec5e5be08a42f14bc19a3f4e
theamik <amik.sylhet@gmail.com>
2023-11-03 12:22:57 +0600

    subscribe

--- routes/partyRoutes.js
+++ routes/partyRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { singleUpload } from "../middlewares/multer.js";
-import { authorizedAdmin, authorizedSubscribers, isAuthenticated } from "../middlewares/auth.js";
+import { authorizedAdmin, isAuthenticated } from "../middlewares/auth.js";
 import { createParty, getAllParties, getAparty } from "../controllers/partyController.js";
 
 const router = express.Router();
@@ -9,14 +9,5 @@ router.route("/parties").get(getAllParties);
 router.route("/party-details").post(getAparty);
 //Create new course
 router.route("/create-party").post(isAuthenticated, authorizedAdmin, singleUpload, createParty);
-
-// router.route("/course/:id")
-//     .get(isAuthenticated, authorizedSubscribers, getCourseLectures)
-//     .post(isAuthenticated, authorizedAdmin, singleUpload, addLecture)
-//     .delete(isAuthenticated, authorizedAdmin, deleteCourse);
-
-
-// //Delete Lecture
-// router.route("/lecture").delete(isAuthenticated, authorizedAdmin, deleteLecture);
 
 export default router;
